504bd2624b99cdc4689942b44de8817a853e76e2
Peter Vavra <peter.vavra.1984@gmail.com>
2020-11-11 14:25:44 +0100

    duplicate code and reference as resp2tsv and puls2tsv

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # procedure_log2tsv
-datalad procedure to preprocess .log files for datalad-hirni 
+Addon for `datalad-hirni` for converting Siemens' physiological .log files into BIDS-format. 
 
+There are two procedures, one for the `..PULS.log` files, and the second one for the `..RESP.log` files. 
 
-For log2tsv.py to work, ```pandas``` and ```DataLad``` have to be installed.
+Each procedure can be used in two modes:
+In mode 1, the procedure simply writes out the resulting tsv-file into the specified directory. The filename will match the input's file, except for the file ending. This mode is intended for asserting that the content of the files is correct, when applying it for the first time in a new project (The file-format of the log-files was reverse engineered and is not relying on any "official" documents by Siemens, so it could be wrong. user beware..).
+In mode 2, the procedure requires additional BIDS-information, like subject number, session, etc., to name and place the file according to BIDS-specs. This latter mode is best used directly via Hirni's `studyspec.json` file. 
 
-The procedure is called by running ```datalad run-procedure log2tsv.py <SOUCE OF .LOG FILES> <LOCATION FOR .TSV FILES>``` in the root dataset.
+The names of the procedures are `puls2tsv` and `resp2tsv`. 
 
-TODO: find out, why code only runs correctly as procedure when called as ```mat2tsv.py```
+To run in mode 1, call `datalad run-procedure puls2tsv input_file_PULS.log output_folder`.
+
+To run in mode 2, specify the procedure in the studyspec.json file, and then call `datalad hirni_spec2bids path_to_studyspec.json`. 
---
+++ code/procedures/resp2tsv.py
@@ -0,0 +1,263 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  13 17:22:07 2020
+
+@author: Judith Bomba
+"""
+import os
+import sys
+import pandas as pd
+from datalad.distribution.dataset import require_dataset
+
+#apparently the call structure for python automatically takes the current dataset (root?) as positional argument number two
+#to resolve said dataset and to make the procedure truly universally run-able this seems to be necessary
+ds = require_dataset(
+    sys.argv[1],
+    check_installed=True,
+    purpose='converting .mat to .tsv files')
+
+inputs = sys.argv[:]
+#print('inputs are: ',inputs)
+input1 = sys.argv[2] #my_source
+input2 = sys.argv[3] #my_destination
+print('will gather .log files from source:', input1, 'will convert to .tsv files stored in:', input2)
+
+root = os.path.dirname(os.path.abspath('sourcedata'))
+
+
+def merge_two_dicts(x, y):
+    '''
+    Given two dictionaries of the same depth,
+        merge them into a new dict as a shallow copy.
+        https://stackoverflow.com/questions/38987/how-do-i-merge-two-dictionaries-in-a-single-expression-in-python-taking-union-o
+
+    Parameters
+    ----------
+    x : dict
+        first dictionary.
+    y : dict
+        second dictionary.
+
+    Returns
+    -------
+    finaldict : TYPE
+        combined dictionary.
+
+    '''
+    finaldict = x.copy()
+    finaldict.update(y)
+    return finaldict
+
+def logInfo_to_tsv(filenm, destination):
+    '''
+    Parameters
+    ----------
+    filenm : os path
+        points to the appropriate file (..Info.log).
+    filesource : os path
+        DESCRIPTION.
+    destination : os path
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    '''
+    file = open(filenm, 'r')
+    logfile = file.read().splitlines()
+
+    ##############################################################################
+    dict_var_set_1 = {}
+    var_set_1 = ([(logfile[i].split())[0] for i in range(7)])
+    var_inhalt_1 = ([(logfile[i].split())[2] for i in range(7)])
+
+    for i in range(len(var_set_1)):
+        k = var_set_1[i]
+        val = var_inhalt_1[i]
+        dict_var_set_1[k] = val
+    #print(dict_var_set_1) #dictionary mit den ersten sechs Metavariablen
+
+    vlastT = ((logfile[8261].split())[2])
+    vfirstT = ((logfile[8260].split())[2])
+
+    dict_var_set_1[(logfile[8261].split())[0]] = vlastT
+    dict_var_set_1[(logfile[8260].split())[0]] = vfirstT
+    ##############################################################################
+    dict_var_set_2 = {}
+    var_set_2 = logfile[8].split()
+
+    for i in range(len(var_set_2)):
+        k = var_set_2[i]
+        for j in range(10,8260):
+            val = (logfile[j].split())
+            val1 = val[i]
+            if j == 10:
+                dict_var_set_2[k] = [val1]
+            else:
+                dict_var_set_2[k].append(val1)
+
+    ##############################################################################
+    finaldict = merge_two_dicts(dict_var_set_1,dict_var_set_2)
+    #print(finaldict.keys())
+
+
+    filename = "{}.tsv".format(filenm[-73:-4])
+    df = pd.DataFrame.from_dict(finaldict)
+    #print(df)
+    df.to_csv('{}/{}'.format(destination,filename), sep = '\t', index=False)
+
+def logPULS_to_tsv(filenm, destination):
+    '''
+    Parameters
+    ----------
+    file : os path
+        points to the appropriate file (..PULS.log).
+
+    Returns
+    -------
+    None.
+
+    '''
+    file = open(filenm, 'r') #filenm
+    logfile = file.read().splitlines()
+
+    dict_var_set_1 = {}
+    var_set_1 = ([(logfile[i].split())[0] for i in range(5)])
+    var_inhalt_1 = ([(logfile[i].split())[2] for i in range(5)])
+
+    for i in range(len(var_set_1)):
+        k = var_set_1[i]
+        val = var_inhalt_1[i]
+        dict_var_set_1[k] = val
+    dict_var_set_2 = {}
+    var_set_2 = logfile[6].split()
+
+    for i in range(len(var_set_2)):
+        k = var_set_2[i]
+        if i != (len(var_set_2)-1):
+            for j in range(8,13418):
+                val = (logfile[j].split())
+                val1 = val[i]
+                if j == 8:
+                    dict_var_set_2[k] = [val1]
+                else:
+                    dict_var_set_2[k].append(val1)
+        else:
+            for j in range(8,13418):
+                if len(logfile[j].split()) == 4:
+                    val = (logfile[j].split())
+                    val1 = val[i]
+                    if j == 8:
+                        dict_var_set_2[k] = [val1]
+                    else:
+                        dict_var_set_2[k].append(val1)
+                else:
+                    val = "NA"
+                    if j == 8:
+                        dict_var_set_2[k] = [val]
+                    else:
+                        dict_var_set_2[k].append(val)
+    finaldict = merge_two_dicts(dict_var_set_1,dict_var_set_2)
+    #print(finaldict.keys())
+
+
+    filename = "{}.tsv".format(filenm[-73:-4])
+    df = pd.DataFrame.from_dict(finaldict)
+    #print(df)
+    df.to_csv('{}/{}'.format(destination,filename), sep = '\t', index=False)
+
+def logRESP_to_tsv(filenm, destination):
+    '''
+    Parameters
+    ----------
+    file : os path
+        points to the appropriate file (..RESP.log).
+
+    Returns
+    -------
+    None.
+
+    '''
+    file = open(filenm, 'r') #filenm
+    logfile = file.read().splitlines()
+    dict_var_set_1 = {}
+    var_set_1 = ([(logfile[i].split())[0] for i in range(5)])
+    var_inhalt_1 = ([(logfile[i].split())[2] for i in range(5)])
+
+    for i in range(len(var_set_1)):
+        k = var_set_1[i]
+        val = var_inhalt_1[i]
+        dict_var_set_1[k] = val
+    dict_var_set_2 = {}
+    var_set_2 = logfile[6].split()
+
+    for i in range(len(var_set_2)):
+        k = var_set_2[i]
+        if i != (len(var_set_2)-1):
+            for j in range(8,13418):
+                val = (logfile[j].split())
+                val1 = val[i]
+                if j == 8:
+                    dict_var_set_2[k] = [val1]
+                else:
+                    dict_var_set_2[k].append(val1)
+        else:
+            for j in range(8,13418):
+                if len(logfile[j].split()) == 4:
+                    val = (logfile[j].split())
+                    val1 = val[i]
+                    if j == 8:
+                        dict_var_set_2[k] = [val1]
+                    else:
+                        dict_var_set_2[k].append(val1)
+                else:
+                    val = "NA"
+                    if j == 8:
+                        dict_var_set_2[k] = [val]
+                    else:
+                        dict_var_set_2[k].append(val)
+    finaldict = merge_two_dicts(dict_var_set_1,dict_var_set_2)
+    #print(finaldict.keys())
+
+
+    filename = "{}.tsv".format(filenm[-73:-4])
+    df = pd.DataFrame.from_dict(finaldict)
+    #print(df)
+    df.to_csv('{}/{}'.format(destination,filename), sep = '\t', index=False)
+
+
+
+filesource = os.path.join(root, input1)
+#print(filesource)
+destination = os.path.join(root, input2)
+
+#print('#these are my files in root:\n',os.listdir(root))
+#print('#these are my files in {}:\n'.format(input1),os.listdir('{}/my_source'.format(root))) #my_s
+print('#these are my files in {} before converting:\n'.format(input2),os.listdir(destination)) #my_d
+#print(root)
+
+for file in os.listdir(filesource):
+    if file.endswith('Info.log'):
+        filename = (os.path.join(filesource,file))
+        #print(filename)
+        print(filename[-73:-4], "attempted to be processed")
+        logInfo_to_tsv(filename, destination)
+
+for file in os.listdir(filesource):
+    if file.endswith('RESP.log'):
+        filename = (os.path.join(filesource,file))
+        #print(filename)
+        print(filename[-73:-4], "attempted to be processed")
+        logRESP_to_tsv(filename, destination)
+
+for file in os.listdir(filesource):
+    if file.endswith('PULS.log'):
+        filename = (os.path.join(filesource,file))
+        #print(filename)
+        print(filename[-73:-4], "attempted to be processed")
+        logPULS_to_tsv(filename, destination)
+
+print('#these are the files in {} after converting:\n'.format(input2),os.listdir(destination))
+
+ds.save(path='.',message='convert .log files from source ({}) to .tsv files and store them in ({})'.format(input1,input2))
